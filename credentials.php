c121382e6aa201530d2c11ba0da7628f70f241b7
Pozinux <tim.poznanski@gmail.com>
2020-11-26 19:18:33 +0100

    update creds

--- credentials.php
+++ credentials.php
@@ -1,7 +1,7 @@
 <?php
 	define("HOSTDB", "127.0.0.1"); // The host you want to connect to.
-	define("USERDB", "userdb"); // The database username.
-	define("DATABASEDB", "cnotdemouserdb"); // The database name
+	define("USERDB", "cnotdemouserdb"); // The database username.
+	define("DATABASEDB", "cnotdemo"); // The database name
 	define("PASSWORDDB", "password"); // The database password. 
 	define("JOURNAL_NAME", "CnoT"); // The Journal name.
 	define("APPPASSWORD", "password"); // The Journal password. 
