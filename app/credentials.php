3dcfa0f742f15d846e3c9b423e3a1af3fb143741
Timothé Poznanski <tim.poznanski@gmail.com>
2021-01-04 13:38:42 +0100

    Delete credentials.php

--- app/credentials.php
+++
@@ -1,8 +0,0 @@
-<?php
-        define("HOSTDB", "127.0.0.1"); // The host you want to connect to.
-        define("USERDB", "userdb"); // The database username.
-        define("DATABASEDB", "cnot"); // The database name
-        define("PASSWORDDB", "XXXXXXXX"); // The database password. 
-        define("JOURNAL_NAME", "CnoT"); // The Journal name.
-        define("APPPASSWORD", "Z"); // The Journal password. 
-?>
