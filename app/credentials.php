9e2f7a63d6d1390c433c33f922cb4cea999abede
Pozinux <tim.poznanski@gmail.com>
2021-01-04 14:21:49 +0100

    create credential file

--- app/credentials.php
+++ app/credentials.php
@@ -4,5 +4,5 @@
         define("DATABASEDB", "cnot"); // The database name
         define("PASSWORDDB", "XXXXXXXX"); // The database password. 
         define("JOURNAL_NAME", "CnoT"); // The Journal name.
-        define("APPPASSWORD", "YYYYYY"); // The Journal password. 
+        define("APPPASSWORD", "ZZZZZ"); // The Journal password. 
 ?>
