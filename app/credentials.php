4c41f21243fb30847d6e8eb4f10e093612acffdb
Timothé Poznanski <tim.poznanski@gmail.com>
2021-01-04 13:11:06 +0100

    Create credentials.php

---
+++ app/credentials.php
@@ -0,0 +1,8 @@
+<?php
+        define("HOSTDB", "127.0.0.1"); // The host you want to connect to.
+        define("USERDB", "userdb"); // The database username.
+        define("DATABASEDB", "cnot"); // The database name
+        define("PASSWORDDB", "XXXXXXXX"); // The database password. 
+        define("JOURNAL_NAME", "CnoT"); // The Journal name.
+        define("APPPASSWORD", "ZZZZZZZ"); // The Journal password. 
+?>
