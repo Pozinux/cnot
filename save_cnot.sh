fc16536d61404149eac4d317b716a8ad0b82a27e
Pozinux <tim.poznanski@gmail.com>
2021-03-10 11:12:02 +0100

    update

--- save_cnot.sh
+++ save_cnot.sh
@@ -14,7 +14,7 @@ app_backup_file="backup_app_${app_name}_${date_heure}.targz"
 # Sauvegarde de l'appli 
 echo "Starting backup of the app files..."
 cd /var/www/html/
-tar czvf ${backup_folder}/${app_backup_file} ${app_folder}
+tar --exclude='cnot/.git' -czvf ${backup_folder}/${app_backup_file} ${app_folder}
 echo "Backup appli done! Backup file is ${backup_folder}${app_backup_file}"
  
 # Sauvegarde de la bdd
