c82baf9a9362d1a46f35c264e08db6d294e07e4f
Pozinux <tim.poznanski@gmail.com>
2021-05-07 22:44:07 +0200

    test ssh key git

--- app/index.php
+++ app/index.php
@@ -7,12 +7,12 @@ session_start();
 	include 'credentials.php';
 	$pass=$_SESSION['pass'];
 	$search = $_POST['search'];
-    $tags_search = $_POST['tags_search'];
-    $dossier = $_GET['dossier'];
-    $sousdossier = $_GET['sousdossier'];
-    $dossier_search = $_POST['dossier_search'];
-    $sousdossier_search = $_POST['sousdossier_search'];
-    $note = $_GET['note'];
+        $tags_search = $_POST['tags_search'];
+        $dossier = $_GET['dossier'];
+        $sousdossier = $_GET['sousdossier'];
+        $dossier_search = $_POST['dossier_search'];
+        $sousdossier_search = $_POST['sousdossier_search'];
+        $note = $_GET['note'];
 	
 	$limite_affichage_droite = 25;
     
